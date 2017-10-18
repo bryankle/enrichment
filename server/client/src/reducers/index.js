d9a7c6d23fcaa7cf4b2e1868b0a3fe1d90e864ad
Bryan Le <bryan@bryanle.io>
2017-10-17 22:50:07 -0400

    Created root reducer

---
+++ server/client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import campusReducer from './campus_reducer';
+import studentReducer from './student_reducer';
+
+const rootReducer = combineReducers({
+	campus: campusReducer,
+	student: studentReducer
+});
+
+export default rootReducer;
