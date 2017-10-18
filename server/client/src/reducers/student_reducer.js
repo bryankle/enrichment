518d9db08cc5fbb81c619c0c2274963e62dcc64a
Bryan Le <bryan@bryanle.io>
2017-10-18 14:36:05 -0400

    Delete student reducer

--- server/client/src/reducers/student_reducer.js
+++ server/client/src/reducers/student_reducer.js
@@ -10,6 +10,13 @@ export default (state = {}, action) => {
 	switch(action.type) {
 		case FETCH_ALL_STUDENTS:
 			return action.payload
+		case DELETE_STUDENT:
+			const idToDelete = action.payload;
+			return {...state, allStudents: state.allStudents.filter((item) => {
+				console.log(item);
+				console.log(item.id, 'and', idToDelete);
+				return item.id !== idToDelete
+			})}
 		default:
 			return state
 	}
