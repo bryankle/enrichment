654c95f3f2e49a7c465dad1d778d3544b91dbb02
Bryan Le <bryan@bryanle.io>
2017-10-18 11:03:17 -0400

    Modal created for edit button in student card

--- server/client/src/components/Cards.js
+++ server/client/src/components/Cards.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { Menu, Card, Image, Icon, Grid, Button } from 'semantic-ui-react'
-
+import { Menu, Card, Image, Icon, Grid, Button, Modal } from 'semantic-ui-react'
+import Modals from './Modals';
 
 // Refactor to functon later
 
 class Cards extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showModal: false
+		}
+	}
+
+	handleEditButton() {
+
+	}
+
 	render() {
 		return(
 			<Card>
+			
 		      <Image src="http://lorempixel.com/400/200" />
 			    <Card.Content>
 			      <Card.Header>
@@ -16,7 +28,6 @@ class Cards extends Component {
 			      <Card.Meta>
 			        <span className='date'>
 			          Description
-
 			        </span>
 			      </Card.Meta>
 			      <Card.Description>
@@ -25,7 +36,7 @@ class Cards extends Component {
     			</Card.Content>
 		      <Card.Content extra>
 		        <div className='ui two buttons'>
-		          <Button basic color='green'>Edit</Button>
+		          <Modals />
 		          <Button basic color='red'>Delete</Button>
 		        </div>
 		      </Card.Content>
